3d78a43cf6610bf26e6579ba2ab49ab40b3c7261
KentChen1112 <100269223+KentChen1112@users.noreply.github.com>
2022-05-19 00:00:05 +0800

    Add files via upload

---
+++ hw8-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Button/>
+    <Input/>
+    </div>
+  );
+}
+
+export default App;
---
+++ hw8-app/src/Components/Email.js
@@ -0,0 +1,5 @@
+import Input from '@mui/material/Input';
+
+<div>
+    <Input/>
+</div>
